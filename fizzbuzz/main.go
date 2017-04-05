b548c5edf09205773555ade79066991f7799650b
ba0918 <pub.dev.delusions@gmail.com>
2017-04-06 01:59:23 +0900

    数値をfizzbuzzに変換する部分を関数として抽出

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -2,25 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	fizzbuzz(1, 100)
+	fizzbuzzRange(1, 100)
 }
 
-func fizzbuzz(start int, end int) {
+func fizzbuzzRange(start int, end int) {
 	for i := start; i < end; i++ {
-		a := i%3 == 0
-		b := i%5 == 0
-		switch {
-		case a && b:
-			fmt.Println("fizzbuzz")
-		case a:
-			fmt.Println("fizz")
-		case b:
-			fmt.Println("buzz")
-		default:
-			fmt.Printf("%d\n", i)
-		}
+		fmt.Println(fizzbuzz(i))
+	}
+}
+
+func fizzbuzz(n int) string {
+	a := n%3 == 0
+	b := n%5 == 0
+	switch {
+	case a && b:
+		return "fizzbuzz"
+	case a:
+		return "fizz"
+	case b:
+		return "buzz"
+	default:
+		return strconv.Itoa(n)
 	}
 }
